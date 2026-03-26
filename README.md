2cdc9a4e30e46d1c0b0a5b4e6baff043db5294d1
Gabriel Freire <154006297+Gabrielfreireflores@users.noreply.github.com>
2026-03-26 16:54:09 -0300

    first Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_schoolproject
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_schoolproject/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_schoolproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
